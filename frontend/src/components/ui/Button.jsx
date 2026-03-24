b47609454caf1c09e37fa0b4deb08cb75cf1502c
unknown <hmy1304@kbu.ac.kr>
2026-03-24 19:49:39 +0900

    update

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Button.scss'
 
-const Button = ({text,className,icons}) => {
+const Button = ({text,className,icons, onClick}) => {
   return (
-    <button className={`btn ${className}`}>
+    <button className={`btn ${className}`} onClick={onClick}>
       {text}
       {icons && <img src='/images/arrow.svg'/>}
     </button>
