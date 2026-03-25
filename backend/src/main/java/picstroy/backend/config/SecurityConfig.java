1e56259d33108154c1c0e112bc5b8fd006091bd9
unknown <hmy1304@kbu.ac.kr>
2026-03-25 20:57:26 +0900

    update

--- backend/src/main/java/picstroy/backend/config/SecurityConfig.java
+++ backend/src/main/java/picstroy/backend/config/SecurityConfig.java
@@ -14,38 +14,55 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    @Value("${cors.allowed-original}")
+    @Value("${cors.allowed-origins}")
     private String allowedOrigins;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/members","/members/**").permitAll()
-                        .requestMatchers("/auth/**").permitAll()
-                        .anyRequest().authenticated()
+                                .requestMatchers("/members","/members/**").permitAll()
+                                .requestMatchers("/auth/**").permitAll()
+//                        .anyRequest().authenticated()
+                                .anyRequest().permitAll()
                 )
                 .formLogin(form->form.disable())
-                .httpBasic(basic->basic.disable());
+                .httpBasic(basic -> basic.disable());
+
         return http.build();
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
 
         config.setAllowedOrigins(List.of(allowedOrigins));
 
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
 
         config.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/**",config);
 
         return source;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
